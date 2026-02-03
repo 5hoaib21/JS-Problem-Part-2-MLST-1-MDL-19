c5bbc269c42fad2548ae1e19f8522cd156cd92f6
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-02-03 20:36:20 +0600

    second commit

--- max.js
+++ max.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+function getMax(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+const max1 = getMax(96, 76);
+const max2 = getMax("01", 2);
+console.log("max of two is:", max1, typeof max2);
